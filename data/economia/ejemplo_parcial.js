b6890d69d0dbbebcdd564b7f7bc6257d73bfa69c
ziro <ziro6142@gmail.com>
2025-09-30 22:18:37 -0300

    terminado?

--- data/economia/ejemplo_parcial.js
+++ data/economia/ejemplo_parcial.js
@@ -31,6 +31,71 @@ const Prototipo_1er_parcial_economía_Preguntas_desarrollo = [
     }
 ];
 
+const Respuestas_de_Desarrollo = [
+    {
+        text: "<strong>Pregunta 1</strong><br>" +
+              "1. Desarrolle qué entiende por modelo económico, cuál es su utilidad y particularidades, ejemplificando a partir del modelo económico mostrado en la imagen.<br>" +
+              "2. Mencione cuál es este modelo, describa todos los agentes que se observan, mencionando sus funciones o características y las interrelaciones entre ellos.<br>" +
+              "3. Pensando en el mercado de bienes y servicios explique qué variables se determinan en las interacciones de este mercado, y qué factores hacen que estas cambien."
+    },
+    {
+        text: "<strong>Pregunta 2</strong><br>" +
+              "A continuación se muestra un gráfico del índice del PIB a precios corrientes de dos países.<br>" +
+              "1. Defina crecimiento económico y explique los indicadores que se usan para medir este fenómeno.<br>" +
+              "2. Mencione las causas o determinantes próximos del crecimiento y desarrolle cada uno explicando claramente su vínculo con el crecimiento económico.<br>" +
+              "3. A partir de la información presentada en el gráfico, relacione la volatilidad de corto plazo con el crecimiento de largo plazo. Explique los conceptos mencionados.<br>" +
+              "4. ¿Qué características ha presentado el ciclo económico de Uruguay y la región en comparación con el de los países desarrollados?"
+    },
+    {
+        text: "<strong>Respuesta Pregunta 2.2</strong><br>" +
+              "Los determinantes próximos del crecimiento económico son dos: aumento de recursos productivos y aumento de la productividad. El aumento de recursos productivos implica acumular capital físico y/o humano. Por lo que es directo ver que si se tienen más capital físico y/o humano, la producción de bienes y servicios va a aumentar. El aumento de la productividad tiene que ver con las mejoras de tecnologías, si se logra una mejora tecnológica es posible aumentar la producción de bienes y servicios, con la misma cantidad de factores productivos."
+    },
+    {
+        text: "<strong>Respuesta Pregunta 2.3</strong><br>" +
+              "Un ciclo económico es una secuencia más o menos regular de recesiones y recuperaciones de la producción real en torno a una senda de crecimiento de largo plazo. Se caracterizan por su duración (tiempo que transcurre entre dos picos de auge o depresión consecutivos) y amplitud (diferencia entre los valores de pico de auge o depresión y la línea de tendencia).<br><br>" +
+              "Las economías con ciclos cortos y profundos son más volátiles. La mayor volatilidad se asocia a mayor inestabilidad e incide negativamente sobre la tasa de crecimiento de largo plazo. Existe, entonces, una correlación negativa entre la inestabilidad de corto plazo que presenta un país y su crecimiento de largo plazo.<br><br>" +
+              "En el gráfico se observa que el país A muestra ciclos más estables (menos volátiles) que el país B, reflejándose en una tasa de crecimiento mayor en el largo plazo."
+    },
+    {
+        text: "<strong>Respuesta Pregunta 2.4</strong><br>" +
+              "En general Uruguay y los países de la región presentan ciclos económicos más volátiles que los países en desarrollo, con fases más cortas y más profundas. Las economías desarrolladas muestran menor volatilidad (ciclos más “suavizados”) y una tendencia más marcada al crecimiento que los países latinoamericanos."
+    },
+    {
+        text: "<strong>Pregunta 3</strong><br>" +
+              "1. A partir del Sistema de Cuentas Nacionales explique el primer gráfico detallando cómo se vinculan estos componentes y su variación. Qué conclusión se obtiene a partir de esto sobre el crecimiento de la economía uruguaya en 2022.<br>" +
+              "2. Analice la variación por sectores de actividad y su vínculo con el desempeño de la economía en ese período.<br>" +
+              "3. ¿Cómo se relaciona la evolución del crecimiento económico con las posibilidades de desarrollo del país en el futuro?"
+    },
+    {
+        text: "<strong>Respuesta Pregunta 3.1</strong><br>" +
+              "El Sistema de Cuentas Nacionales es un sistema de registro contable de la actividad económica de una economía basado en las equivalencias básicas. A partir de este se produce información sobre las variables económicas relevantes: Producto Interno Bruto (PIB), Consumo Privado (C), Gasto del Gobierno (G), Inversión o Formación Bruta de Capital Fijo (I), Exportaciones (X) e Importaciones (M).<br><br>" +
+              "Siguiendo las equivalencias básicas del modelo del flujo circular complejo para una economía abierta, la Oferta agregada es igual a la Demanda agregada. Por lo que: PIB + M = C + I + G + X.<br><br>" +
+              "La economía uruguaya creció 4,9% en 2022 con respecto a 2021 y este crecimiento se explica por la variación en el resto de las variables. En particular, el crecimiento económico fue impulsado por el crecimiento de las exportaciones que aumentaron un 11,1%, la inversión (creció 5,2%), el consumo de los hogares (aumentó 6%) y el gasto del gobierno (aumentó 0,6%)."
+    },
+    {
+        text: "<strong>Respuesta Pregunta 3.3</strong><br>" +
+              "La evolución del crecimiento económico se vincula estrechamente con las posibilidades de desarrollo futuro de un país, ya que constituye la base material sobre la cual se pueden sustentar mejoras en el bienestar de la población. Sin embargo, el crecimiento por sí solo no garantiza el desarrollo. Para que exista desarrollo económico el proceso de crecimiento económico debe estar acompañado de un aumento de las capacidades productivas del país.<br><br>" +
+              "A su vez, considerando otras dimensiones del desarrollo, es decir para que el crecimiento económico se relacione con una mejora en la calidad de vida de las personas, las decisiones de cada país sobre la orientación de su estructura productiva, la inversión en capital humano a través de la educación y la salud, el impulso a la innovación tecnológica y la forma de inserción en los mercados internacionales son factores determinantes. Además, el crecimiento económico sólo se traduce en desarrollo si se acompaña de políticas redistributivas y de reducción de la pobreza, que permitan que los beneficios lleguen de manera equitativa al conjunto de los habitantes.<br><br>" +
+              "Las posibilidades de desarrollo de un país en el futuro no dependen únicamente del crecimiento económico que logre alcanzar, sino también de cómo gestione dicho crecimiento, orientándolo hacia la inclusión social, la equidad y la sostenibilidad."
+    },
+    {
+        text: "<strong>Respuesta Pregunta 1.1</strong><br>" +
+              "Un modelo económico es una descripción simplificada de la realidad, que rescata la lógica de funcionamiento de aquello que quiere describir, así como también los determinantes. Para lograr esto, los modelos suelen hacer supuestos y explicitar cómo son las relaciones entre las variables económicas relevantes para el modelo. Son útiles ya que logran de manera simplificada, describir un fenómeno complejo. Por ejemplo, en el modelo de flujo circular complejo se logra describir el funcionamiento general de una economía. Algo que podría parecer muy complejo, se logra reducir al esquema presentado."
+    },
+    {
+        text: "<strong>Respuesta Pregunta 1.2</strong><br>" +
+              "El modelo de la imagen es el modelo de flujo circular complejo, los agentes son los hogares, las empresas, el sector público y el sector externo. Para una descripción detallada de funciones o características de los agentes, y sus interrelaciones ver diapositivas 22 a 41 de la clase 1."
+    },
+    {
+        text: "<strong>Respuesta Pregunta 1.3</strong><br>" +
+              "En el mercado de bienes y servicios, como en cualquier mercado en general, se determinan precios y cantidades de bienes y servicios. El precio y la cantidad transada de un determinado bien y servicio se ven afectados por aquellos factores que determinan la oferta y la demanda. Detrás de la oferta de un mercado se encuentran las empresas, que son quienes producen y venden (ofertan) el bien o servicio, detrás de la demanda se encuentran los hogares, que son quienes van al mercado de bienes y servicios en busca de estos para satisfacer sus necesidades de consumo. Por lo tanto, los factores que determinan la oferta están relacionados con aquellos que afectan a las empresas, y los que determinan la demanda están relacionados con aquellos que afectan a los hogares."
+    },
+    {
+        text: "<strong>Respuesta Pregunta 2.1</strong><br>" +
+              "El crecimiento económico es un proceso de largo plazo en el que se incrementa la producción de bienes y servicios. Para medirlo se utilizan los indicadores: PIB real y PIB real per cápita."
+    }
+];
+
 const Prototipo_1er_parcial_economía_Preguntas_múltiple_opción = [
     {
         text: "<strong>1.</strong> ¿Por qué entiende usted que la actual división entre países ricos y pobres nació a partir de los viajes de descubrimiento y posterior conquista europeos durante los siglos XV y XVI?<br>" +
@@ -111,12 +176,31 @@ const Prototipo_1er_parcial_economía_Preguntas_múltiple_opción = [
     }
 ];
 
+const Respuestas_múltiple_opción_Prototipo_1er_parcial_economía = [
+    {
+        text: "<strong>Respuestas múltiple opción</strong><br>" +
+              "1. c<br>" +
+              "2. a<br>" +
+              "3. d<br>" +
+              "4. b<br>" +
+              "5. c<br>" +
+              "6. b<br>" +
+              "7. b<br>" +
+              "8. a<br>" +
+              "9. b<br>" +
+              "10. b<br>" +
+              "11. c"
+    }
+];
 
 export const temas = {
     tema: "Ejemplo_parcial",
     temas:{
         'primer_parcial_economia_preguntas_desarrollo': Prototipo_1er_parcial_economía_Preguntas_desarrollo,
-        'primer_parcial_economia_preguntas_multiples_opciones': Prototipo_1er_parcial_economía_Preguntas_múltiple_opción
+        'respuestas_de_desarrollo': Respuestas_de_Desarrollo,
+        'primer_parcial_economia_preguntas_multiples_opciones': Prototipo_1er_parcial_economía_Preguntas_múltiple_opción,
+        'respuestas_multiples_opciones': Respuestas_múltiple_opción_Prototipo_1er_parcial_economía
+        
         
         // Añade más temas aquí siguiendo el patrón: 'nombreTema': nombreTema_constante
         // Asegúrate de que las claves (strings) coincidan con los nombres de las constantes definidas
